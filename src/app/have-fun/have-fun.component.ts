39cb3542ebdf12bc7c7a6a816652ded629e81fb8
wan-zhen <kelly811124811124@hotmail.com>
2018-06-15 17:51:56 +0800

    have fun subnav & npm audio fix

--- src/app/have-fun/have-fun.component.ts
+++ src/app/have-fun/have-fun.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-have-fun',
@@ -9,29 +16,33 @@ import { HttpClient } from '@angular/common/http';
 export class HaveFunComponent implements OnInit {
   recordsData: IRecordArray[];
   zoneData: string[] = [];
-  constructor(private http: HttpClient) { }
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+  constructor(
+    private http: HttpClient,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit() {
     const params = { q: '', limit: 15 };
     const api = `https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97`;
-    this.http.get<IApi>(api)
-      .subscribe(arg => {
-        console.log(arg.result.records);
-        this.recordsData = arg.result.records;
-        this.recordsData.forEach(v => {
-          this.zoneData.push(v.Zone);
-          if (v.Ticketinfo === '' || v.Ticketinfo === '免費參觀') {
-            v.free = true;
-          }
-          if (v.Opentime === '全天候開放') {
-            v.allOpen = true;
-          }
-        });
-        this.zoneData = this.zoneData.filter((v, i, arr) => arr.indexOf(v) === i);
-        console.log(this.zoneData);
+    this.http.get<IApi>(api).subscribe(arg => {
+      console.log(arg.result.records);
+      this.recordsData = arg.result.records;
+      this.recordsData.forEach(v => {
+        this.zoneData.push(v.Zone);
+        if (v.Ticketinfo === '' || v.Ticketinfo === '免費參觀') {
+          v.free = true;
+        }
+        if (v.Opentime === '全天候開放') {
+          v.allOpen = true;
+        }
       });
+      this.zoneData = this.zoneData.filter((v, i, arr) => arr.indexOf(v) === i);
+      console.log(this.zoneData);
+    });
   }
-
 }
 
 interface IApi {
